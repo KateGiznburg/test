85f7525db1899d243f80bc7bcd649e2342c0854d
KateGiznburg <60586902+KateGiznburg@users.noreply.github.com>
2022-06-13 10:35:20 +0200

    Add files via upload

--- script2.ps1
+++ script2.ps1
@@ -1,4 +1,4 @@
-Install-Module AzureAD
-Import-Module AzureAD
+Install-Module -Name "AzureAD" -Scope CurrentUser
+Import-Module -Name "AzureAD"
 Connect-AzureAD
 Remove-AzureADServicePrincipal -ObjectId 861fb414-8773-49c6-912b-46de5d91d1cf
